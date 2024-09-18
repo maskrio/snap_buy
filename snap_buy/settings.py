67efb834ef6c564d2250eeb7eab4a6e1a10a6f44
maskrio <yosemaranata@gmail.com>
2024-09-18 19:40:21 +0700

    selesai tugas4

--- snap_buy/settings.py
+++ snap_buy/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-3t$w@38%g7e3wx!v8)ee_!)$&r=nic0#$_u+q+(5!oj-g6s5+v'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+PRODUCTION = os.getenv("PRODUCTION", False)
+DEBUG = not PRODUCTION
 
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "yose-yehezkiel-snapbuy.pbp.cs.ui.ac.id"]
 
